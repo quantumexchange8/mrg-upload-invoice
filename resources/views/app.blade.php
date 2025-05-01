1ef581a2d90525227c18e2fdf533d6e7151f4649
unknown <demondrake00@gmail.com>
2025-05-01 14:32:01 +0800

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -20,7 +20,7 @@
 
         <!-- Scripts -->
         @routes
-        @vite(['resources/js/app.js', "resources/js/pages/{$page['Components']}.vue"])
+        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
